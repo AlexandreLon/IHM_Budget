768fa864b74c50645ab65101c71eaa972e8a71df
Mehdi <mehdi.cherkaoui@etu.unice.fr>
2019-02-09 12:41:29 +0100

    #1 Add budget management

--- src/program/controller/HomeController.java
+++ src/program/controller/HomeController.java
@@ -1,87 +1,27 @@
 package program.controller;
-
-import program.model.Department;
-import program.model.Internship;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableRow;
-import javafx.scene.control.TableView;
-import javafx.scene.input.MouseButton;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
+import javafx.scene.chart.PieChart;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-
+/**
+ * @author CHERKAOUI Mehdi
+ */
 
 public class HomeController {
-   // private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+    @FXML
+    private PieChart pieChart;
 
     @FXML
-    private TableView<Department> depTable;
+    public void initialize(URL url, ResourceBundle rbl){
+        ObservableList<PieChart.Data> pieChartData
+                = FXCollections.observableArrayList(
+                new PieChart.Data("Budget", 190),
+                new PieChart.Data("Plafond", 300));
 
-    @FXML
-    private TableColumn<Department, String> tag;
-
-    @FXML
-    private TableColumn<Department, String> name;
-
-    @FXML
-    private TableColumn<Department, String> room;
-
-    @FXML
-    public void initialize() {
-        List<Internship> siInternships = Arrays.asList(new Internship("SI3: Stage ouvrier", "Stage de 3 semaines minimum en été"),
-                new Internship("SI4: Stage développeur", "Stage de 5 semaines minimum en été"),
-                new Internship("SI5: Stage ingénieur", "Stage de 5 mois minimum entre mars et septembre"));
-        List<Internship> elecInternships = Arrays.asList(new Internship("SI3: Stage ouvrier", "Stage de 3 semaines minimum en été"),
-                new Internship("SI4: Stage technicien", "Stage de 5 semaines minimum en été"),
-                new Internship("SI5: Stage ingénieur", "Stage de 5 mois minimum entre mars et septembre"));
-
-        ObservableList<Department> departments = FXCollections.observableArrayList();
-        departments.add(new Department("SI", "Sciences Informatiques", "O+110", Color.BLUEVIOLET, siInternships));
-        departments.add(new Department("ELEC", "Electronique", "O+111", Color.ORANGE, elecInternships));
-
-        tag.setCellValueFactory(cellData -> cellData.getValue().tagNameProperty());
-        name.setCellValueFactory(cellData -> cellData.getValue().nameProperty());
-        room.setCellValueFactory(cellData -> cellData.getValue().meetingRoomProperty());
-
-        depTable.setItems(departments);
-        depTable.setRowFactory(tv -> {
-            TableRow<Department> row = new TableRow<>();
-            row.setOnMouseClicked(event -> {
-                if (!row.isEmpty() && event.getButton() == MouseButton.PRIMARY) {
-                    displayDepartment(row.getItem());
-                }
-            });
-            return row;
-        });
+        pieChart.setData(pieChartData);
     }
-
-    private void displayDepartment(Department department) {
-        String fxmlFile = "../../resources/fxml/polytech_department.fxml";
-        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
-        try {
-            Stage stage = (Stage) depTable.getScene().getWindow();
-            Parent rootNode = loader.load(getClass().getResourceAsStream(fxmlFile));
-
-            Scene scene = new Scene(rootNode);
-            stage.setScene(scene);
-            ((DepartmentController) loader.getController()).initDepartement(department);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 
 }
