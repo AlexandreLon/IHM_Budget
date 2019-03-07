eddb795c7b3020471d2cefc9b1fc644480dd7025
Alexandre Lon <alexandre.longordo@etu.univ-cotedazur.fr>
2019-03-07 20:22:24 +0100

    Create useless folder to know what are files use and not use
    If need use a useless page, move it

--- src/program/view/View.java
+++ src/program/view/View.java
@@ -7,10 +7,8 @@ package program.view;
  */
 public abstract class View {
     public static final String HOME = "/resources/fxml/Home.fxml";
-    public static final String CONFIRM  = "/resources/fxml/Alert.fxml";
     public static final String CONNECTION = "/resources/fxml/Connection.fxml";
     public static final String CREATEACCOUNT  = "/resources/fxml/CreateAccount.fxml";
-    public static final String RECIPES  =  "/resources/fxml/Recipes.fxml";
     public static final String HISTORY  =  "/resources/fxml/History.fxml";
 
 }
